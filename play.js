b29cae9c555caafa53f61be81f07a0c93800afce
jacksnow811 <jack2605097@gmail.com>
2021-05-16 23:01:59 +0800

    1.新增歌曲 2.將radio的資源重新改寫，避免每次換歌單都必須重新載入所有的歌曲。

--- play.js
+++ play.js
@@ -26,28 +26,29 @@ let playNow;
 let SliderRange = document.querySelector('#SliderRange');
 let music_name = document.querySelector("#music_name");
 let music_time = document.querySelector("#music_time");
-
+let MusicList = document.querySelector("#MusicList");
 //let timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
-
+/*
 class playNow_object {
     constructor(AudioPlayer, MusicName) {
         this.AudioPlayer = AudioPlayer;
         this.MusicName = MusicName;
     }
 }
+*/
 
-(function () {
-    document.querySelector('#MusicList').addEventListener('change', function () {
-        if (this.value === 'Japanese')
-            InitMediaList(mediaJapanese);
-        else if (this.value === 'English')
-            InitMediaList(mediaEnglish);
+(function () {     
+    MusicList.addEventListener('change', function () {
+        InitMediaList(this.value)
     })
     document.querySelector('#SliderRange').addEventListener('change',(event)=>{
-        playNow.AudioPlayer.currentTime=Math.round(playNow.AudioPlayer.duration*event.target.value/100)
+        if(playNow.AudioPlayer.duration>0)
+        {
+            playNow.AudioPlayer.currentTime=Math.round(playNow.AudioPlayer.duration*event.target.value/100)
+        }
     })
 
-    InitMediaList(mediaJapanese);
+    InitMediaList(MusicList.value);
 
     let play = document.querySelector('#Div_play').addEventListener('click', PlayHandler)    
     let pause = document.querySelector('#Div_pause').addEventListener('click', PauseHandler)
@@ -55,7 +56,20 @@ class playNow_object {
     let foreward = document.querySelector('#foreward').addEventListener('click', ForwardHandler)
 
 })();
+function InitMediaList(InListName){
+    if (playNow !== undefined) {
+        if(playNow.AudioPlayer!==null)
+        {
+            PauseHandler();
+            playNow.AudioPlayer.currentTime=0;
+        }
+    }
+    musicArray=PlayTotalAry[PlayTotalAry.findIndex(e=>e.ListName==InListName)].play_objectAry;
+    playNow = musicArray[0];
+    music_name.innerHTML = playNow.MusicName;
+}
 
+/*
 function InitMediaList(MediaArray) {
     if (playNow !== undefined) {
         PauseHandler();
@@ -65,8 +79,6 @@ function InitMediaList(MediaArray) {
     for (let a = 0; a < MediaArray.length; a++) {
         var audioPlayer = new Audio(MediaArray[a].fileUrl);
         audioPlayer.addEventListener("timeupdate", function () {
-            //var playPercent = timelineWidth * (this.currentTime / this.duration);
-            //playhead.style.width = playPercent + "px";
             let Min = Math.floor(this.currentTime / 60)
             let sec = Math.floor(this.currentTime % 60)
             if (sec <= 9) {
@@ -74,8 +86,7 @@ function InitMediaList(MediaArray) {
             } else {
                 music_time.innerHTML = Min + ":" + sec;
             }
-            SliderRange.value=this.currentTime / this.duration * 100 ;         
-
+            SliderRange.value=this.currentTime / this.duration * 100 ;       
         }, false);
         audioPlayer.addEventListener("ended", ForwardHandler);
         let array = MediaArray[a].fileUrl.split('/');
@@ -85,14 +96,14 @@ function InitMediaList(MediaArray) {
     playNow = musicArray[0];
     music_name.innerHTML = playNow.MusicName;
 }
+*/
 
 function PlayHandler() {
     //if (playNow == undefined)
     //    playNow = musicArray[0];
     document.querySelector('#Div_play').style.display = 'none';
     document.querySelector('#Div_pause').style.display = '';
-    playNow.AudioPlayer.play();
-    music_name.innerHTML = playNow.MusicName;
+    ChangePlaymusic(null,musicArray[ musicArray.findIndex(e => e == playNow)])
 }
 
 function PauseHandler() {
@@ -100,18 +111,51 @@ function PauseHandler() {
     document.querySelector('#Div_pause').style.display = 'none';
     playNow.AudioPlayer.pause();
 }
-
+/*InitMediaList
 function ChangePlaymusic(playNow_object_old, playNow_object_new) {
     playNow_object_old.AudioPlayer.pause();
     playNow_object_old.AudioPlayer.currentTime = 0;
     if (playNow_object_new == undefined)
-        return;
+        return;    
     playNow_object_new.AudioPlayer.play();
     music_name.innerHTML = playNow_object_new.MusicName;
     playNow = playNow_object_new;
     document.querySelector('#Div_play').style.display = 'none';
     document.querySelector('#Div_pause').style.display = '';
 }
+*/
+function ChangePlaymusic(play_object_old, play_object_new) {
+    if(play_object_old!==null)
+    {
+        if(play_object_old.AudioPlayer!==null){
+            play_object_old.AudioPlayer.pause();
+            play_object_old.AudioPlayer.currentTime = 0; 
+        }
+    }
+    if (play_object_new == undefined)
+        return;
+    if(play_object_new.AudioPlayer ==null) {
+        var audioPlayer = new Audio(play_object_new.fileUrl);
+        audioPlayer.addEventListener("timeupdate", function () {
+            let Min = Math.floor(this.currentTime / 60)
+            let sec = Math.floor(this.currentTime % 60)
+            if (sec <= 9) {
+                music_time.innerHTML = Min + ":0" + sec;
+            } else {
+                music_time.innerHTML = Min + ":" + sec;
+            }
+            SliderRange.value=this.currentTime / this.duration * 100 ;       
+        }, false);
+        audioPlayer.addEventListener("ended", ForwardHandler);
+        play_object_new.AudioPlayer=audioPlayer;
+    }
+    play_object_new.AudioPlayer.play();
+    music_name.innerHTML = play_object_new.MusicName;
+    playNow = play_object_new;
+    document.querySelector('#Div_play').style.display = 'none';
+    document.querySelector('#Div_pause').style.display = '';
+}
+
 
 function ForwardHandler() {
     let index = musicArray.findIndex(e => e == playNow);
--- playList.js
+++ playList.js
@@ -1,2 +1,45 @@
 const mediaEnglish = [{ fileUrl: "sounds/English/Beautiful Lyrics Eminem.mp3" }, { fileUrl: "sounds/English/Diddy - Dirty Money - Coming Home ft. Skylar Grey (Official Video).mp3" }, { fileUrl: "sounds/English/Disney's Frozen Let It Go Sequence Performed by Idina Menzel.mp3" }, { fileUrl: "sounds/English/Dr. Dre - I Need A Doctor (Explicit) ft. Eminem, Skylar Grey Lyrics.mp3" }, { fileUrl: "sounds/English/Edward Maya - Stereo Love (Dubstep Remix).mp3" }, { fileUrl: "sounds/English/Eminem - Mockingbird (Official Music Video).mp3" }, { fileUrl: "sounds/English/Imagine Dragons - Radioactive.mp3" }, { fileUrl: "sounds/English/Imagine Dragons - Ready Aim Fire Hereos Fall Soundtrack.mp3" }, { fileUrl: "sounds/English/Justin Timberlake - Mirrors.mp3" }, { fileUrl: "sounds/English/Katy Perry  Dark Horse Official ft Juicy J.mp3" }, { fileUrl: "sounds/English/Little Talks - Of Monsters and Men - Official Acoustic Music Video- Julia Sheer & Jon D - on iTunes.mp3" }, { fileUrl: "sounds/English/Lorde - Team (Lyric Video).mp3" }, { fileUrl: "sounds/English/Mika -  Happy ending lyrics.mp3" }, { fileUrl: "sounds/English/Monster (Lyrics) - Imagine Dragons.mp3" }, { fileUrl: "sounds/English/OneRepublic - Counting Stars (Official Music Video).mp3" }, { fileUrl: "sounds/English/Pink -Try (Lyrics).mp3" }, { fileUrl: "sounds/English/Rhye - Open.mp3" }, { fileUrl: "sounds/English/Ryan Star - Start A Fire (lyrics).mp3" }, { fileUrl: "sounds/English/Safe  Sound feat The Civil Wars The Hunger Games Songs From District 12 And Beyond.mp3" }, { fileUrl: "sounds/English/Skylar Grey  Coming home part 2  Lyrics.mp3" }, { fileUrl: "sounds/English/The Script If you could see me now lyrics.mp3" }, { fileUrl: "sounds/English/Train- Drive by (with lyrics).mp3" }, { fileUrl: "sounds/English/拉娜德芮 大亨小傳 電影主題曲 中文MV.mp3" }]
 const mediaJapanese =[{fileUrl:"sounds/Japanese/『僕のヒーローアカデミア』×米津玄師「ピースサイン」スペシャルミュージックビデオ.mp3"},{fileUrl:"sounds/Japanese/米津玄師  MV「 灰色と青（ ＋菅田将暉 ）」.mp3"},{fileUrl:"sounds/Japanese/米津玄師  MV「Lemon」.mp3"},{fileUrl:"sounds/Japanese/米津玄師  MV「アイネクライネ」.mp3"},{fileUrl:"sounds/Japanese/米津玄師  MV「アンビリーバーズ」.mp3"},{fileUrl:"sounds/Japanese/米津玄師  MV「リビングデッド・ユース」.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「Flamingo」.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「Flowerwall」.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「LOSER」.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「orion」.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「TEENAGE RIOT」.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「カムパネルラ」.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「パプリカ」Kenshi Yonezu - Paprika.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「ピースサイン」Kenshi Yonezu - Peace Sign.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「フローライト」.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「メトロノーム」.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「感電」.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「春雷」Shunrai.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「海の幽霊」Spirits of the Sea.mp3"},{fileUrl:"sounds/Japanese/米津玄師 MV「馬と鹿」Uma to Shika.mp3"},{fileUrl:"sounds/Japanese/米津玄師　MV「サンタマリア」.mp3"},{fileUrl:"sounds/Japanese/米津玄師　MV『vivi』.mp3"},{fileUrl:"sounds/Japanese/米津玄師　MV『恋と病熱』.mp3"}];
+const mediaEnglish2=[{fileUrl:"sounds/English2/Alan Walker - All Falls Down (feat. Noah Cyrus with Digital Farm Animals).mp3"},{fileUrl:"sounds/English2/Alan Walker - Darkside (feat. AuRa and Tomine Harket).mp3"},{fileUrl:"sounds/English2/Alan Walker - Diamond Heart (feat. Sophia Somajo).mp3"},{fileUrl:"sounds/English2/Alan Walker - Faded.mp3"},{fileUrl:"sounds/English2/Alan Walker - Sing Me To Sleep.mp3"},{fileUrl:"sounds/English2/Avicii - Waiting For Love.mp3"},{fileUrl:"sounds/English2/Call You Mine.mp3"},{fileUrl:"sounds/English2/Clean Bandit 清潔的盜賊 - Symphony feat. Zara Larsson 莎拉萊森 (華納 official HD 官方完整版 MV).mp3"},{fileUrl:"sounds/English2/Coldplay - Adventure Of A Lifetime (Official Video).mp3"},{fileUrl:"sounds/English2/Coldplay - Paradise (Official Video).mp3"},{fileUrl:"sounds/English2/Ed Sheeran 紅髮艾德 - Galway Girl 高威女孩 (華納official HD 高畫質官方中字版).mp3"},{fileUrl:"sounds/English2/Jonas Blue - Rise ft. Jack Jack.mp3"},{fileUrl:"sounds/English2/K-391 Alan Walker - Ignite feat. Julie Bergan Seungri (Lyric Video).mp3"},{fileUrl:"sounds/English2/Linkin Park 聯合公園 - One More Light 又一道光芒 (華納official HD 高畫質官方中字版).mp3"},{fileUrl:"sounds/English2/Lukas Graham - 7 Years [OFFICIAL MUSIC VIDEO].mp3"},{fileUrl:"sounds/English2/Mama.mp3"},{fileUrl:"sounds/English2/Martin Garrix Troye Sivan - There For You (Official Video).mp3"},{fileUrl:"sounds/English2/R. City ft. Adam Levine - Locked Away (Official Video).mp3"},{fileUrl:"sounds/English2/Shawn Mendes - Treat You Better.mp3"},{fileUrl:"sounds/English2/Sia - The Greatest.mp3"},{fileUrl:"sounds/English2/The Chainsmokers - All We Know ft. Phoebe Ryan (Audio).mp3"},{fileUrl:"sounds/English2/The Chainsmokers - Beach House (Official Video).mp3"},{fileUrl:"sounds/English2/The Chainsmokers - Paris (Official Music Video).mp3"},{fileUrl:"sounds/English2/The Chainsmokers - Roses ft. ROZES (Lyric Video).mp3"},{fileUrl:"sounds/English2/The Chainsmokers - This Feeling ft. Kelsea Ballerini (Lyric Video).mp3"},{fileUrl:"sounds/English2/The Chainsmokers - Young (Audio).mp3"},{fileUrl:"sounds/English2/Zedd - Beautiful Now (Official Music Video) ft. Jon Bellion.mp3"},{fileUrl:"sounds/English2/Zedd, Alessia Cara - Stay (Lyric Video).mp3"},{fileUrl:"sounds/English2/《Fast Furious 8 The Album》G-Eazy Kehlani 柯蘭妮 - Good Life 美好人生 (華納 Official 完整MV).mp3"},{fileUrl:"sounds/English2/老菸槍雙人組 The Chainsmokers ft. Daya - 別讓我失望 Dont Let Me Down 中文字幕 MV.mp3"},{fileUrl:"sounds/English2/老菸槍雙人組 The Chainsmokers 厭世男孩 Sick Boy (HD中字MV).mp3"},{fileUrl:"sounds/English2/馬汀蓋瑞克斯 Martin Garrix 與 Bebe Rexha 以愛之名 In The Name Of Love (HD中字MV).mp3"}];
+const mediaESO=[{fileUrl:"sounds/ESO/Fail With You (feat. 42).mp3"},{fileUrl:"sounds/ESO/她沒在看我.mp3"},{fileUrl:"sounds/ESO/未接來電.mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【CHANGE】Official Music Video(4K).mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【Dont Worry About Me】.mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【Follow You】Official Music Video(4K).mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【Hello Beautiful】Official Music Video.mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【I Wish I Was There】Official Music Video.mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【Money Bag】.mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【So Good To Me】.mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【Something I Dont Need】.mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【WAIT】Official Music Video.mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【伯父 Bo Fu】Official Music Video.mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【太陽】.mp3"},{fileUrl:"sounds/ESO/瘦子E.SO【稱讚她的美】.mp3"},{fileUrl:"sounds/ESO/若即若離.mp3"},{fileUrl:"sounds/ESO/迷途羔羊.mp3"},{fileUrl:"sounds/ESO/頑童MJ116【2030】Official Music Video.mp3"}];
+
+let PlayTotalAry=[];
+class playListObject
+{
+    constructor(ListName, play_objectAry) {
+        this.ListName = ListName;
+        this.play_objectAry = play_objectAry;
+    }
+}
+
+class play_object 
+{
+    constructor(MusicName,fileUrl,AudioPlayer,InListName) {
+        this.AudioPlayer = AudioPlayer;
+        this.MusicName = MusicName;
+        this.fileUrl = fileUrl;
+        this.InListName = InListName;
+    }
+}
+
+(function(){
+    CreateObjectByList(mediaEnglish2,'English2');
+    CreateObjectByList(mediaEnglish,'English');
+    CreateObjectByList(mediaJapanese,'Japanese');
+    CreateObjectByList(mediaESO,'ESO');
+})();
+
+function CreateObjectByList(mediaAry,ListName){
+    let play_objectAry=[];    
+    for(let i=0;i<mediaAry.length;i++){
+        let array=mediaAry[i].fileUrl.split('/');
+        MusicName=array[array.length - 1];
+        play_objectAry.push(new play_object(MusicName,mediaAry[i].fileUrl,null,ListName))
+    }
+    PlayTotalAry.push(new playListObject(ListName,play_objectAry));
+
+    var x = document.querySelector("#MusicList");
+    let option = document.createElement("option");
+    option.text = ListName;
+    x.add(option);
+}
